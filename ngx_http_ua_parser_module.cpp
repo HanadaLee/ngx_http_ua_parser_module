785f39976f9e24b9cb84b99f33dc529091b5ef7c
Hanada <im@hanada.info>
2025-07-29 00:30:13 +0800

    fix: update ngx_http_ua_parser_regexes_file to retrieve main conf correctly

--- ngx_http_ua_parser_module.cpp
+++ ngx_http_ua_parser_module.cpp
@@ -408,15 +408,17 @@ ngx_http_ua_parser_create_main_conf(ngx_conf_t *cf)
 static char *
 ngx_http_ua_parser_regexes_file(ngx_conf_t *cf, ngx_command_t *cmd, void *conf)
 {
-    auto conf = static_cast<ngx_http_ua_parser_main_conf_t *>(data);
+    auto conf = static_cast<ngx_http_ua_parser_main_conf_t *>(
+        ngx_http_conf_get_module_main_conf(cf, ngx_http_ua_parser_module)
+    );
 
-    ngx_str_t     *value;
+    ngx_str_t      *value;
 
     if (conf->parser != NULL) {
-        return "is duplicate";
+        return const_cast<char *>("is duplicate");
     }
 
-    value = cf->args->elts;
+    value = static_cast<ngx_str_t *>(cf->args->elts);
 
     if (value[1].data && value[1].data[0] != '/') {
         if (ngx_conf_full_name(cf->cycle, &value[1], 0) != NGX_OK) {
